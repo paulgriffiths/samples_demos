e01e6b7cf5f5c267e2e69359c926a3cccebe4863
Paul Griffiths <mail@paulgriffiths.net>
2014-11-08 12:04:59 -0500

    Add gds_assert_quit() function.

--- data_structures/generic/gds_util.c
+++ data_structures/generic/gds_util.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <assert.h>
 #include "gds_util.h"
 
 void gds_strerror_quit(const char * msg, ...)
@@ -36,4 +38,25 @@ void gds_error_quit(const char * msg, ...)
     }    
 
     exit(EXIT_FAILURE);
+}
+
+void gds_assert_quit(const char * msg, ...)
+{
+
+#ifndef NDEBUG
+
+    fprintf(stderr, "gds library error: ");
+
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+
+    if ( msg[strlen(msg) - 1] != '\n' ) {
+        fputc('\n', stderr);
+    }    
+
+#endif
+
+    assert(false);
 }
--- data_structures/generic/gds_util.h
+++ data_structures/generic/gds_util.h
@@ -3,5 +3,6 @@
 
 void gds_strerror_quit(const char * msg, ...);
 void gds_error_quit(const char * msg, ...);
+void gds_assert_quit(const char * msg, ...);
 
 #endif      /*  PG_SAMPLES_AND_DEMOS_GDS_UTIL_H  */
